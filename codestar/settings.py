e58a91b9ccd8cfb83e536d8f25643731b30bb5c0
David Bergqvist <David.bergqvist@gmail.com>
2024-05-10 14:01:12 +0000

    added the about page with content

--- codestar/settings.py
+++ codestar/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = os.environ.get("SECRET_KEY")
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['.herokuapp.com','8000-davidb3rgqvi-djangoblog-oslt6t033g9.ws-eu110.gitpod.io', '8000-davidb3rgqvi-djangoblog-oslt6t033g9.ws-eu111.gitpod.io']
 
@@ -44,6 +44,7 @@ INSTALLED_APPS = [
     'django.contrib.staticfiles',
     'django_summernote',
     'blog',
+    'about',
 ]
 
 MIDDLEWARE = [
