8fa665775e0f35e304ebfa1f18f14774c52275ac
David Bergqvist <David.bergqvist@gmail.com>
2024-05-14 09:04:29 +0000

    added contact form to the about page

--- blog/models.py
+++ blog/models.py
@@ -1,9 +1,13 @@
 from django.db import models
 from django.contrib.auth.models import User
 
-# Create your models here.
 STATUS = ((0, "Draft"), (1, "Published"))
 
+# Create your models here.
+
+def profile_page(request):
+    user = get_object_or_404(User, user=request.user)
+    comments = user.commenter.all()
 
 class Post(models.Model):
     title = models.CharField(max_length=200, unique=True)
@@ -27,8 +31,7 @@ class Post(models.Model):
 class Comment(models.Model):
     post = models.ForeignKey(Post, on_delete=models.CASCADE,
                              related_name="comments")
-    name = models.CharField(max_length=80)
-    email = models.EmailField()
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     body = models.TextField()
     created_on = models.DateTimeField(auto_now_add=True)
     approved = models.BooleanField(default=False)
@@ -37,4 +40,4 @@ class Comment(models.Model):
         ordering = ["created_on"]
 
     def __str__(self):
-        return f"Comment {self.body} by {self.name}"
+        return f"Comment {self.body} by {self.author}"
